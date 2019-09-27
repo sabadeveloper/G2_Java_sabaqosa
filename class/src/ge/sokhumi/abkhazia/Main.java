083bc747bb0e3fe0a64b4b64052cecf37d7c4c61
saba_kosa <saba.kosashvili.1@btu.edu.ge>
2019-09-27 12:40:22 +0400

    klasswork

---
+++ class/src/ge/sokhumi/abkhazia/Main.java
@@ -0,0 +1,15 @@
+package ge.sokhumi.abkhazia;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hello");
+        teststudent student = new teststudent();
+        student.name = "davit";
+        student.lastName="mainsuradze";
+        student.printData();
+
+    }
+    
+}
---
+++ class/src/ge/sokhumi/abkhazia/teststudent.java
@@ -0,0 +1,12 @@
+package ge.sokhumi.abkhazia;
+
+public class teststudent {
+    String name;
+    public String lastName;
+    private int age;
+
+    public void printData(){
+        System.out.println(name+" "+lastName+" "+age);
+
+    }
+}
