cdca7d15388c7b74b581ce853e1d22bed316b4f5
sabadeveloper <35997679+sabadeveloper@users.noreply.github.com>
2019-09-29 23:39:38 +0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        C main = new C();
+        main.Method1();
+        //
+        // main.Method2();
+        //main.Method3();
+       // main.Method4();
+        //main.Method5();
+        main.Method6();
+
+
+
+
+    }
+}
